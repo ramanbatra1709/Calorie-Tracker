54405b4c2d29f0ce7e92f3a71e66cc837a448352
Raman Batra <raman.batra1709@gmail.com>
2018-11-09 05:01:26 +0530

    Add controllers (using the module pattern)

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const ItemController = (function(){
+    const Item = function(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+    const state = {
+        items: [
+            {
+                id: 0,
+                name: 'Dinner',
+                calories: 1200
+            },
+            {
+                id: 1,
+                name: 'Cookies',
+                calories: 400
+            },
+            {
+                id: 2,
+                name: 'Eggs',
+                calories: 300
+            }
+        ],
+        currentItem: null,
+        totalCalories: 0
+    };
+
+    return {
+        getState: function() {
+            return state;
+        }
+    };
+})();
+
+const UIController = (function(){
+    return  {
+        
+    };
+})();
+
+const AppController = (function(ItemController, UIController){
+    return {
+        init: function()    {
+
+        }
+    };
+})(ItemController, UIController);
+
+AppController.init();
